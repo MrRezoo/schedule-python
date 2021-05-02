8959253381fa06c458bfa7fbc0590aef5aa29137
MrRezoo <rezam578@gmail.com>
2021-05-02 13:48:12 +0430

    create every method

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-
-
 class Schedule:
-    pass
+    def __init__(self):
+        self.job = []
+
+    def every(self, interval=1):
+        job = Job(interval)
+        self.job.append(job)
+        return job
 
 
 class Job:
-    pass
+    def __init__(self, interval):
+        self.interval = interval
+        self.job_dunc = None
+        self.last_run = None
+        self.next_run = None
+        self.unit = None
+        self.period = None
